183b9b04fa90dfbd42febf1a1e5429734b551942
mihalicf <mihalicf@ae.com>
2022-05-31 16:46:12 +0200

    Final changes and transalations

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,84 +1,221 @@
 module.exports = {
-    // General
-    website: {
-      'en': 'Website',
-      'sr': 'Veb Stranica'
-    },
-    headerLinkHome: {
-      'en': 'Home',
-      'sr': 'Početna'
-    },
-    headerLinkAbout: {
-      'en': 'About',
-      'sr': 'O Nama'
-    },
-    headerLinkAboutItem1: {
-      'en': 'About Us',
-      'sr': 'O Nama'
-    },
-    headerLinkAboutItem2: {
-      'en': 'The Team',
-      'sr': 'Naš Tim'
-    },
-    headerLinkServices: {
-      'en': 'Practice Areas',
-      'sr': 'Oblasti Rada'
-    },
-    headerLinkContact: {
-      'en': 'Contact',
-      'sr': 'Kontakt'
-    },
-		ourFirm: {
-			'en': 'Kostić Law Office was founded in 1968 by Miloš Kostić - attorney at law (1939-2021). During many years of legal practice, the office primarily focus was on civil, sports commercial and IP law. As a result of many years of cooperation, it merged with the Law Office Pecikoza, whose founder, Jelena Mihalic Pecikoza - attorney at law, has many years of experience in the fields of real estate, commercial and IP Law.',
-			'sr': 'Advokatska kancelarija Kostić osnovana je 1968. godine od strane advokata Miloša Kostića (1939-2021). U svojoj bogatoj advokatskoj praksi kancelarija se bavila primarno građanskim pravom, sportskim pravom, privrednim pravom i pravom intelektualne svojine. Kao rezultat dugogodišnje saradnje, došlo je do spajanja sa Advokatskom kancelarijom Pecikoza, ciji osnivac advokat Jelena Mihalic Pecikoza poseduje dugogdisnje iskustvo rada u oblastima imovinsko-pravnih odnosa, privrednog i autorskog prava.'
-
+	// General
+	website: {
+		'en': 'Website',
+		'sr': 'Veb Stranica'
+	},
+	slogan: {
+		'sr': 'Tradicija. Efikasnost. Posvećenost.',
+		'en': 'Tradition. Efficiency. Dedication.'
+	},
+	headerLinkHome: {
+		'en': 'Home',
+		'sr': 'Početna'
+	},
+	headerLinkAbout: {
+		'en': 'About',
+		'sr': 'O Nama'
+	},
+	headerLinkAboutItem1: {
+		'en': 'About Us',
+		'sr': 'O Nama'
+	},
+	headerLinkAboutItem2: {
+		'en': 'The Team',
+		'sr': 'Naš Tim'
+	},
+	headerLinkServices: {
+		'en': 'Practice Areas',
+		'sr': 'Oblasti Rada'
+	},
+	headerLinkContact: {
+		'en': 'Contact',
+		'sr': 'Kontakt'
+	},
+	home: {
+		cta: {
+			'en': 'Now! Make an Appointment.',
+			'sr': 'Odmah! Zakažite konsultacije.'
 		},
-    practiceAreasCommercial: {
-      'en': 'Commercial Law',
-      'sr': 'Privredno pravo'
-    },
-    practiceAreasIp: {
-      'en': 'Intellectual Property Law',
-      'sr': 'Pravo intelektualne svojine'
-    },
-    practiceAreasLabour: {
-      'en': 'Labour Law',
-      'sr': 'Radno pravo'
-    },
-    practiceAreasTax: {
-      'en': 'Tax Law',
-      'sr': 'Poresko pravo'
-    },
-    practiceAreasFamily: {
-      'en': 'Family and Inheritance Law',
-      'sr': 'Porodično i nasledno pravo'
-    },
-    practiceAreasProperty: {
-      'en': 'Property Law',
-      'sr': 'Imovinsko pravo'
-    },
-    practiceAreasSport: {
-      'en': 'Sports Law',
-      'sr': 'Sportsko pravo'
-    },
-    practiceAreasBankruptcy: {
-      'en': 'Bankruptcy Law',
-      'sr': 'Stečajno pravo'
-    },
-    practiceAreasDamage: {
-      'en': 'Damage Compensation',
-      'sr': 'Naknada štete'
-    },
-    practiceAreasContract: {
-      'en': 'Contract Law',
-      'sr': 'Obligaciono pravo'
-    },
-    practiceAreasOpinions: {
-      'en': 'Drafting Legal Opinions',
-      'sr': 'Izrada pravnih mišljenja'
-    },
-    practiceAreasCriminal: {
-      'en': 'Criminal and Misdemeanor Law',
-      'sr': 'Krivično i prekršajno pravo'
-    },
-  };
+		sendUsAMessage: {
+			'en': 'Send us a message',
+			'sr': 'Pošaljite nam poruku'
+		},
+		callUs: {
+			'en': 'Call Us!',
+			'sr': 'Pozovite nas!'
+		},
+	},
+	experiences: {
+		title: {
+			'sr':'Iskustvo',
+			'en':'Experience',
+		},
+		punchline: {
+				firstPart: {
+				'en': 'Let Our Experience',
+				'sr': 'Dozvolite da Vas naše',
+				},
+				secondPart: {
+					'sr': 'iskutvo vodi.',
+					'en': 'Be Your Guide.'
+				}
+		},
+		ourOffice: {
+			title: {
+				'en': 'Our Office',
+				'sr': 'Naša kancelarija'
+			},
+			description: {
+				'en': 'Kostić Law Office was founded in 1968 by Miloš Kostić - attorney at law (1939-2021). During many years of legal practice, the office primarily focus was on civil, sports commercial and IP law. As a result of many years of cooperation, it merged with the Law Office Pecikoza, whose founder, Jelena Mihalic Pecikoza - attorney at law, has many years of experience in the fields of real estate, commercial and IP Law.',
+				'sr': 'Advokatska kancelarija Kostić osnovana je 1968. godine od strane advokata Miloša Kostića (1939-2021). U svojoj bogatoj advokatskoj praksi kancelarija se bavila primarno građanskim pravom, sportskim pravom, privrednim pravom i pravom intelektualne svojine. Kao rezultat dugogodišnje saradnje, došlo je do spajanja sa Advokatskom kancelarijom Pecikoza, ciji osnivac advokat Jelena Mihalic Pecikoza poseduje dugogdisnje iskustvo rada u oblastima imovinsko-pravnih odnosa, privrednog i autorskog prava.'
+			}
+		},
+		ourAttorneys: {
+			title: {
+				'en': 'Our Attorneys',
+				'sr': 'Naši advokati',
+			},
+			description: {
+				'en': 'Our team is comprised of passionate and seasoned attorneys with a mindset of serving you — we are easy to work with and relate to our clients. From the moment we agree to work together until we close the case, every step is formulated with you, the client, in mind.',
+				'sr': 'Naš tim se sastoji od iskusnih i posvećenih advokata čiji je cilj da vam pomognu – izgradnja odnosa i jednostavnost komunikacije sa klijentima je ono što pravi razliku. Od trenutka otpočinjanja saradnje, svaki korak je pažljivo isplaniran zajedno sa vama, jer klijenti su naš prioritet.',
+			}
+		},
+		ourPrinciples: {
+			title: {
+				'en': 'Our Principles',
+				'sr': 'Naša načela',
+			},
+			description: {
+				'en': 'Part of our winning strategy is simply caring more, but it goes much further than that. When you choose our Law Firm, you can be sure that protection of your rights is in good hands.',
+				'sr': 'Deo pobedničke strategije jeste više brige o klijentima - ali i mnogo više od toga. Od trenutka kada izaberete našu advokatsku kancelariju možete biti sigurni da je zaštita vaših prava u sigurnim rukama.',
+			}
+		},
+	},
+	hero: {
+		provenResults: {
+			title: {
+				'en': 'Proven Results',
+				'sr': 'Dokazani rezultati',
+			},
+			content: {
+				'en': 'Our Law Office has provided quality, trusted legal representation to individuals and companies for the past few decades.',
+				'sr': 'Naša advokatska kancelarija je u poslednjih nekoliko decenija obezbedila kvalitetno i poverljivo pravno zastupanje kako pojedincima tako i kompanijama.'
+			}
+		},
+		experienceWhenItMatters: {
+			title: {
+				'en': 'Experience When it Matters',
+				'sr': 'Iskustvo kada je to važno'
+			},
+			content: {
+				'en': 'Getting the best result for your case means having a team of attorneys with experience in the local courts and with domestic authorities.',
+				'sr': 'Postizanje najboljih rezultata za vaš slučaj zahteva tim advokata koji poseduju iskustvo pred lokalnim sudovima i drugim nadležnim organima.'
+			}
+		}
+
+	},
+	intro: {
+		punchline: {
+			'en': 'We strive to positively influence our community by adopting sustainable business practices and by providing fair and transparent legal services. We believe that by using technology to create efficiency in our practice we are able to increase access to high quality legal services for both individuals and business owners.',
+			'sr': 'Nastojimo da izvršima pozitivan uticaj na poslovno okruženje usvajanjem održive poslovne prakse i pružanjem poštenih i transparentnih advokatskih usluga. Upotrebom tehnologije stvaramo efikasnost u obavljanju našeg posla i olakšavamo pristup efikasnoj pravnoj zaštiti kako fizičkim tako i pravnim licima.'
+		},
+
+		welcome: {
+		 'en': 'Welcome',
+		 'sr': 'Dobrodošli',
+		},
+
+		slogan: {
+			firstPart: {
+				'en':'Tradition.',
+				'sr':'Tradicija.'
+			},
+			secondPart: {
+				'en': 'Efficiency. Dedication.',
+				'sr': 'Efikasnost. Posvećenost.'
+			}
+		}
+	},
+	team: {
+		title: {
+			'en': 'Our Lawyer Team',
+			'sr': 'Naš advokatski tim',
+		},
+		partner: {
+			'en': 'Partner',
+			'sr': 'Partner',
+		},
+		consultant: {
+			'en': 'Consultant',
+			'sr': 'Konsultant',
+		},
+		associate: {
+			'en': 'Associate',
+			'sr': 'Pripravnik'
+		}
+	},
+	contact: {
+		title: {
+			'en':'Contact Us',
+			'sr':'Kontaktirajte nas'
+		}
+	},
+	belgrade: {
+		'en': 'Belgrade',
+		'sr': 'Beograd'
+	},
+	//@TODO: refactor this
+	practiceAreasTitle: {
+		'en': 'Practice Areas',
+		'sr': 'Oblasti rada'
+	},
+	practiceAreasCommercial: {
+		'en': 'Commercial Law',
+		'sr': 'Privredno pravo'
+	},
+	practiceAreasIp: {
+		'en': 'Intellectual Property Law',
+		'sr': 'Pravo intelektualne svojine'
+	},
+	practiceAreasLabour: {
+		'en': 'Labour Law',
+		'sr': 'Radno pravo'
+	},
+	practiceAreasTax: {
+		'en': 'Tax Law',
+		'sr': 'Poresko pravo'
+	},
+	practiceAreasFamily: {
+		'en': 'Family and Inheritance Law',
+		'sr': 'Porodično i nasledno pravo'
+	},
+	practiceAreasProperty: {
+		'en': 'Property Law',
+		'sr': 'Imovinsko pravo'
+	},
+	practiceAreasSport: {
+		'en': 'Sports Law',
+		'sr': 'Sportsko pravo'
+	},
+	practiceAreasBankruptcy: {
+		'en': 'Bankruptcy Law',
+		'sr': 'Stečajno pravo'
+	},
+	practiceAreasDamage: {
+		'en': 'Damage Compensation',
+		'sr': 'Naknada štete'
+	},
+	practiceAreasContract: {
+		'en': 'Contract Law',
+		'sr': 'Obligaciono pravo'
+	},
+	practiceAreasOpinions: {
+		'en': 'Drafting Legal Opinions',
+		'sr': 'Izrada pravnih mišljenja'
+	},
+	practiceAreasCriminal: {
+		'en': 'Criminal and Misdemeanor Law',
+		'sr': 'Krivično i prekršajno pravo'
+	},
+};
