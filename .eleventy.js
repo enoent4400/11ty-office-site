1c969c3060a048e70375801684b93e65ba26a747
mihalicf <mihalicf@ae.com>
2022-06-02 01:34:07 +0200

    Add SEO

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 const i18n = require('eleventy-plugin-i18n');
 const translations = require('./src/_data/i18n');
+const fs = require("fs");
+const NOT_FOUND_PATH = "public/404.html";
+
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/css');
@@ -26,6 +29,16 @@ module.exports = function (eleventyConfig) {
             });
             res.end();
           }
+					else if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          } else {
+						const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+						// Add 404 http status code in request header.
+						res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+						// Provides the 404 content without redirect.
+						res.write(content_404);
+						res.end();
+					}
         });
       },
     },
